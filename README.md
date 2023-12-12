94de406fe7b5f9471afcf77d19179e441a8848c3
faizttn <faijuddin@tothenew.com>
2023-12-12 22:01:43 +0530

    generate jwt token

--- README.md
+++ README.md
@@ -98,6 +98,10 @@ $ npm i -D @types/bcrypt
 
 $ npx prisma studio
 
+# jwt
+$ npm i @nestjs/jwt
+
+
 ## Support
 
 Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,15 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { jwtSecret } from 'src/utils/constants';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisam: PrismaService) {}
+  constructor(
+    private readonly prisam: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
 
   async signup(dto: CreateAuthDto) {
     const { email, password } = dto;
@@ -31,12 +36,27 @@ export class AuthService {
 
     const userExist = await this.prisam.user.findUnique({ where: { email } });
 
-    if (userExist) {
-      const isMatch = await this.comparePassword(password, userExist.password);
-      if (!isMatch) throw new BadRequestException('Invalid credentials');
-    } else {
+    // if (userExist) {
+    //   const isMatch = await this.comparePassword(password, userExist.password);
+    //   if (!isMatch) throw new BadRequestException('Invalid credentials');
+    // } else {
+    //   throw new BadRequestException('Invalid credentials');
+    // }
+
+    if (!userExist) {
       throw new BadRequestException('Invalid credentials');
     }
+
+    const isMatch = await this.comparePassword(password, userExist.password);
+
+    if (!isMatch) throw new BadRequestException('Invalid credentials');
+
+    const token = await this.signToken({
+      id: userExist.id,
+      email: userExist.email,
+    });
+
+    return { message: 'Login successfull', data: { token } };
   }
 
   async signout() {
@@ -48,8 +68,16 @@ export class AuthService {
     return await bcrypt.hash(password, saltOrRounds);
   }
 
-  async comparePassword(password: string, hashed: string) {
+  async comparePassword(password: string, hashed: string): Promise<boolean> {
     return await bcrypt.compare(password, hashed);
+  }
+
+  async signToken(args: { id: string; email: string }) {
+    const payload = args;
+    const token = await this.jwt.signAsync(payload, {
+      secret: jwtSecret,
+    });
+    return token;
   }
 
   findAll() {
